7adc9291ec75d9c0d699e43924951a20514f370e
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-12-06 17:54:16 +0200

    Update Tomcat to new images as well

--- smoke-tests/src/test/java/com/splunk/opentelemetry/TomcatSmokeTest.java
+++ smoke-tests/src/test/java/com/splunk/opentelemetry/TomcatSmokeTest.java
@@ -35,11 +35,11 @@ public class TomcatSmokeTest extends AppServerTest {
 
   private static Stream<Arguments> supportedConfigurations() {
     return Stream.of(
-        arguments("splunk-tomcat:7-jdk8", TOMCAT7_SERVER_ATTRIBUTES),
-        arguments("splunk-tomcat:8-jdk8", TOMCAT8_SERVER_ATTRIBUTES),
-        arguments("splunk-tomcat:8-jdk11", TOMCAT8_SERVER_ATTRIBUTES),
-        arguments("splunk-tomcat:9-jdk8", TOMCAT9_SERVER_ATTRIBUTES),
-        arguments("splunk-tomcat:9-jdk11", TOMCAT9_SERVER_ATTRIBUTES));
+        arguments("ghcr.io/open-telemetry/java-test-containers:tomcat-7.0.107-jdk8", TOMCAT7_SERVER_ATTRIBUTES),
+        arguments("ghcr.io/open-telemetry/java-test-containers:tomcat-8.5.60-jdk8", TOMCAT8_SERVER_ATTRIBUTES),
+        arguments("ghcr.io/open-telemetry/java-test-containers:tomcat-8.5.60-jdk11", TOMCAT8_SERVER_ATTRIBUTES),
+        arguments("ghcr.io/open-telemetry/java-test-containers:tomcat-9.0.40-jdk8", TOMCAT9_SERVER_ATTRIBUTES),
+        arguments("ghcr.io/open-telemetry/java-test-containers:tomcat-9.0.40-jdk11", TOMCAT9_SERVER_ATTRIBUTES));
   }
 
   @ParameterizedTest(name = "[{index}] {0}")

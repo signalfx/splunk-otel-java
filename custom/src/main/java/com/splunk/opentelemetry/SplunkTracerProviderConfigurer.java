9c08b4f64e39913872f523df7567a5f0ae879d06
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-02-02 13:49:02 +0100

    spotless

--- custom/src/main/java/com/splunk/opentelemetry/SplunkTracerProviderConfigurer.java
+++ custom/src/main/java/com/splunk/opentelemetry/SplunkTracerProviderConfigurer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.splunk.opentelemetry;
 
 import com.google.auto.service.AutoService;
@@ -12,7 +28,8 @@ public class SplunkTracerProviderConfigurer implements SdkTracerProviderConfigur
   @Override
   public void configure(SdkTracerProviderBuilder tracerProvider) {
     tracerProvider
-        .setTraceConfig(TraceConfig.builder()
+        .setTraceConfig(
+            TraceConfig.builder()
                 .setMaxNumberOfAttributes(Integer.MAX_VALUE)
                 .setMaxNumberOfEvents(Integer.MAX_VALUE)
                 .setMaxNumberOfLinks(1000) // this is the default value actually

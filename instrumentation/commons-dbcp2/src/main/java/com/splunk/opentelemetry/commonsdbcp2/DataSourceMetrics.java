933c2dd519d1d93ca520aab789bdd78b2b3963b9
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-03-29 13:10:51 +0200

    Add base unit

--- instrumentation/commons-dbcp2/src/main/java/com/splunk/opentelemetry/commonsdbcp2/DataSourceMetrics.java
+++ instrumentation/commons-dbcp2/src/main/java/com/splunk/opentelemetry/commonsdbcp2/DataSourceMetrics.java
@@ -17,27 +17,40 @@
 package com.splunk.opentelemetry.commonsdbcp2;
 
 import com.splunk.opentelemetry.javaagent.bootstrap.GlobalMetricsTags;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.binder.BaseUnits;
 import java.util.List;
+import java.util.function.Supplier;
 import javax.management.ObjectName;
 import org.apache.commons.dbcp2.BasicDataSourceMXBean;
 
 public final class DataSourceMetrics {
   public static void registerMetrics(BasicDataSourceMXBean dataSource, ObjectName objectName) {
+    List<Tag> tags = getTags(objectName);
+
+    gauge("db.pool.connections.active", tags, dataSource::getNumActive);
+    gauge("db.pool.connections.idle", tags, dataSource::getNumIdle);
+    gauge("db.pool.connections.min", tags, dataSource::getMinIdle);
+    gauge("db.pool.connections.max", tags, dataSource::getMaxTotal);
+  }
+
+  private static List<Tag> getTags(ObjectName objectName) {
     // use the "name" property if available: Spring sets it to the bean name
     String name = objectName.getKeyProperty("name");
     // if its unavailable just use the whole mbean name
     if (name == null) {
       name = objectName.toString();
     }
-    List<Tag> tags = GlobalMetricsTags.concat(Tag.of("type", "dbcp2"), Tag.of("name", name));
+    return GlobalMetricsTags.concat(Tag.of("type", "dbcp2"), Tag.of("name", name));
+  }
 
-    Metrics.gauge(
-        "db.pool.connections.active", tags, dataSource, BasicDataSourceMXBean::getNumActive);
-    Metrics.gauge("db.pool.connections.idle", tags, dataSource, BasicDataSourceMXBean::getNumIdle);
-    Metrics.gauge("db.pool.connections.min", tags, dataSource, BasicDataSourceMXBean::getMinIdle);
-    Metrics.gauge("db.pool.connections.max", tags, dataSource, BasicDataSourceMXBean::getMaxTotal);
+  private static void gauge(String name, Iterable<Tag> tags, Supplier<Number> function) {
+    Gauge.builder(name, function)
+        .tags(tags)
+        .baseUnit(BaseUnits.CONNECTIONS)
+        .register(Metrics.globalRegistry);
   }
 
   private DataSourceMetrics() {}

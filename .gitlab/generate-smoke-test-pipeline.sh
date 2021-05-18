405e052ed40d9db6daedd285f60f48f9629752e3
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-05-18 19:10:44 +0200

    Gitlab releaser job

---
+++ .gitlab/generate-smoke-test-pipeline.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+SYSTEMS=("linux" "windows")
+SUITES=("glassfish" "jboss" "jetty" "liberty" "tomcat" "tomee" "weblogic" "wildfly" "other")
+
+cat <<EOF
+.smoke-test: &smoke-test
+  stage: test
+  script:
+    - ./gradlew :smoke-tests:test -PsmokeTestSuite="$SMOKE_TEST_SUITE" --scan --no-daemon
+
+.linux-smoke-test: &linux-smoke-test
+  << *smoke-test
+  image: openjdk:11.0.11-9-jdk
+
+.windows-smoke-test: &windows-smoke-test
+  << *smoke-test
+  image: openjdk:11.0.11-9-jdk-windowsservercore
+EOF
+
+for os in "${SYSTEMS[@]}"
+do
+  for suite in "${SUITES[@]}"
+  do
+    cat << EOF
+
+smoke-test-${os}-${suite}:
+  << *${os}-smoke-test
+  variables:
+    SMOKE_TEST_SUITE: ${suite}
+EOF
+  done
+done

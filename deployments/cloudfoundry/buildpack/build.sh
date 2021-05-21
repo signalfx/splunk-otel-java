05a9addd109de81721d4a367de5faeb987911e14
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-05-21 07:57:13 +0200

    Attach buildpack zip during release process (#241)

--- deployments/cloudfoundry/buildpack/build.sh
+++ deployments/cloudfoundry/buildpack/build.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
 BUILDPACK_RELEASE="splunk_otel_java_buildpack-linux.zip"
 
 zip "$BUILDPACK_RELEASE" bin/* manifest.yml README.md

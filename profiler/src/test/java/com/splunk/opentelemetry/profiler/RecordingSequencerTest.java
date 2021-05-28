d44d6531f570c3063db7956d2880640b25174222
Jason Plumb <jplumb@splunk.com>
2021-05-28 10:20:02 -0700

    spotless apply

--- profiler/src/test/java/com/splunk/opentelemetry/profiler/RecordingSequencerTest.java
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/RecordingSequencerTest.java
@@ -22,7 +22,6 @@ import static org.mockito.Mockito.*;
 
 import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -31,84 +30,57 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class RecordingSequencerTest {
 
-    Duration duration = Duration.ofMillis(10);
+  Duration duration = Duration.ofMillis(10);
 
-    @Mock
-    JfrRecorder recorder;
-    @Mock
-    RecordingEscapeHatch escapeHatch;
+  @Mock JfrRecorder recorder;
+  @Mock RecordingEscapeHatch escapeHatch;
 
-    @Test
-    void canContinueNotStarted() {
-        when(escapeHatch.jfrCanContinue()).thenReturn(true);
-        when(recorder.isStarted()).thenReturn(false);
-        RecordingSequencer sequencer = buildSequencer();
-        sequencer.handleInterval();
-        verify(recorder).start();
-        verifyNoMoreInteractions(recorder);
+  @Test
+  void canContinueNotStarted() {
+    when(escapeHatch.jfrCanContinue()).thenReturn(true);
+    when(recorder.isStarted()).thenReturn(false);
+    RecordingSequencer sequencer = buildSequencer();
+    sequencer.handleInterval();
+    verify(recorder).start();
+    verifyNoMoreInteractions(recorder);
   }
 
-    @Test
-    void canContinueAlreadyStarted() {
-        when(escapeHatch.jfrCanContinue()).thenReturn(true);
+  @Test
+  void canContinueAlreadyStarted() {
+    when(escapeHatch.jfrCanContinue()).thenReturn(true);
     when(recorder.isStarted()).thenReturn(true);
-        RecordingSequencer sequencer = buildSequencer();
-        sequencer.handleInterval();
-        verify(recorder).flushSnapshot();
-        verifyNoMoreInteractions(recorder);
-    }
+    RecordingSequencer sequencer = buildSequencer();
+    sequencer.handleInterval();
+    verify(recorder).flushSnapshot();
+    verifyNoMoreInteractions(recorder);
+  }
 
-    @Test
-    void startThroughFlushSequence() throws Exception {
-        CountDownLatch latch = new CountDownLatch(3);
+  @Test
+  void startThroughFlushSequence() throws Exception {
+    CountDownLatch latch = new CountDownLatch(3);
     when(escapeHatch.jfrCanContinue()).thenReturn(true);
     recorder = new MockRecorder(latch);
-        RecordingSequencer sequencer = buildSequencer();
-        sequencer.start();
-        assertTrue(latch.await(5, SECONDS));
-    }
+    RecordingSequencer sequencer = buildSequencer();
+    sequencer.start();
+    assertTrue(latch.await(5, SECONDS));
+  }
 
-    @Test
-    void testUnhealthyNotStarted() {
-        when(escapeHatch.jfrCanContinue()).thenReturn(false);
-        when(recorder.isStarted()).thenReturn(false);
-        RecordingSequencer sequencer = buildSequencer();
-        sequencer.handleInterval();
+  @Test
+  void testUnhealthyNotStarted() {
+    when(escapeHatch.jfrCanContinue()).thenReturn(false);
+    when(recorder.isStarted()).thenReturn(false);
+    RecordingSequencer sequencer = buildSequencer();
+    sequencer.handleInterval();
     verify(recorder, never()).start();
     verify(recorder, never()).flushSnapshot();
-    }
+  }
 
-    @Test
-    void testUnhealthyStartedCallsStop() {
-        when(escapeHatch.jfrCanContinue()).thenReturn(true);
-        when(recorder.isStarted()).thenReturn(false);
-        RecordingSequencer sequencer = buildSequencer();
+  @Test
+  void testUnhealthyStartedCallsStop() {
+    when(escapeHatch.jfrCanContinue()).thenReturn(true);
+    when(recorder.isStarted()).thenReturn(false);
+    RecordingSequencer sequencer = buildSequencer();
 
-        // First time through we start
-        sequencer.handleInterval();
-        verify(recorder).start();
-        verify(recorder, never()).flushSnapshot();
-        when(recorder.isStarted()).thenReturn(true);
-
-        // Second time through we flush
-        sequencer.handleInterval();
-        verify(recorder).flushSnapshot();
-        verify(recorder, never()).stop();
-
-        // Now we are broken, so we call stop()
-        when(escapeHatch.jfrCanContinue()).thenReturn(false);
-        sequencer.handleInterval();
-        verify(recorder).stop();
-        verify(recorder, times(1)).flushSnapshot();
-        verify(recorder, times(1)).start();
-    }
-
-    @Test
-    void testRecoversAfterSkipping() throws Exception {
-        CountDownLatch flushLatch = new CountDownLatch(3);
-        when(escapeHatch.jfrCanContinue()).thenReturn(true);
-        MockRecorder recorder = new MockRecorder(flushLatch);
-        RecordingSequencer sequencer = buildSequencer(recorder);
     // First time through we start
     sequencer.handleInterval();
     verify(recorder).start();
@@ -133,25 +105,48 @@ class RecordingSequencerTest {
     CountDownLatch flushLatch = new CountDownLatch(3);
     when(escapeHatch.jfrCanContinue()).thenReturn(true);
     MockRecorder recorder = new MockRecorder(flushLatch);
-    RecordingSequencer sequencer = buildSequencer(recorder);    sequencer.start();
-        assertTrue(flushLatch.await(5, SECONDS));
-        assertEquals(1, recorder.stopLatch.getCount());
-        assertTrue(recorder.isStarted());
+    RecordingSequencer sequencer = buildSequencer(recorder);
+    // First time through we start
+    sequencer.handleInterval();
+    verify(recorder).start();
+    verify(recorder, never()).flushSnapshot();
+    when(recorder.isStarted()).thenReturn(true);
 
-        // After 3 flushes, we are now broken!
-        when(escapeHatch.jfrCanContinue()).thenReturn(false);
-        assertTrue(recorder.stopLatch.await(5, SECONDS));
-        assertFalse(recorder.isStarted());
+    // Second time through we flush
+    sequencer.handleInterval();
+    verify(recorder).flushSnapshot();
+    verify(recorder, never()).stop();
 
-        flushLatch = new CountDownLatch(3);
-        recorder.setFlushLatch(flushLatch);
-        // And now we got happy again
-        when(escapeHatch.jfrCanContinue()).thenReturn(true);
-        assertTrue(flushLatch.await(5, SECONDS));
-        assertTrue(recorder.isStarted());
+    // Now we are broken, so we call stop()
+    when(escapeHatch.jfrCanContinue()).thenReturn(false);
+    sequencer.handleInterval();
+    verify(recorder).stop();
+    verify(recorder, times(1)).flushSnapshot();
+    verify(recorder, times(1)).start();
+  }
 
+  @Test
+  void testRecoversAfterSkipping() throws Exception {
+    CountDownLatch flushLatch = new CountDownLatch(3);
+    when(escapeHatch.jfrCanContinue()).thenReturn(true);
+    MockRecorder recorder = new MockRecorder(flushLatch);
+    RecordingSequencer sequencer = buildSequencer(recorder);sequencer.start();
+    assertTrue(flushLatch.await(5, SECONDS));
+    assertEquals(1, recorder.stopLatch.getCount());
+    assertTrue(recorder.isStarted());
 
-    }
+    // After 3 flushes, we are now broken!
+    when(escapeHatch.jfrCanContinue()).thenReturn(false);
+    assertTrue(recorder.stopLatch.await(5, SECONDS));
+    assertFalse(recorder.isStarted());
+
+    flushLatch = new CountDownLatch(3);
+    recorder.setFlushLatch(flushLatch);
+    // And now we got happy again
+    when(escapeHatch.jfrCanContinue()).thenReturn(true);
+    assertTrue(flushLatch.await(5, SECONDS));
+    assertTrue(recorder.isStarted());
+  }
 
   private RecordingSequencer buildSequencer() {
     return buildSequencer(recorder);

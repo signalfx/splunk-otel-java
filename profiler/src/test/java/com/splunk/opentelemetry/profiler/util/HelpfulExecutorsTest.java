9f89de217a1375c9a77dd5581783ab8305e33d45
Jason Plumb <jplumb@splunk.com>
2021-06-01 11:10:38 -0700

    increase visibility into test failure (works on my box)

--- profiler/src/test/java/com/splunk/opentelemetry/profiler/util/HelpfulExecutorsTest.java
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/util/HelpfulExecutorsTest.java
@@ -120,7 +120,10 @@ class HelpfulExecutorsTest {
     seenNames.drainTo(resultNames);
     List<Boolean> resultDaemons = new ArrayList<>();
     seenIsDaemon.drainTo(resultDaemons);
-    assertEquals(Arrays.asList("test!!!", "test!!!", "test!!!"), resultNames);
+    assertEquals(3, resultNames.size());
+    assertEquals("test!!!", resultNames.get(0));
+    assertEquals("test!!!", resultNames.get(1));
+    assertEquals("test!!!", resultNames.get(2));
     assertEquals(Arrays.asList(true, true, true), resultDaemons);
   }
 }

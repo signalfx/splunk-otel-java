fc3441a58ef894cdc1d7066d3229ca6f06a4a03c
Jason Plumb <jplumb@splunk.com>
2021-06-08 16:20:02 -0700

    add filter and sort

---
+++ profiler/src/main/java/com/splunk/opentelemetry/profiler/FilterSortedRecordingFile.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.splunk.opentelemetry.profiler;
+
+import com.splunk.opentelemetry.profiler.events.RelevantEvents;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.stream.Stream;
+import jdk.jfr.consumer.RecordedEvent;
+
+/**
+ * Limits a delegate stream to just the events that we care about and returns them sorted by start
+ * time.
+ */
+public class FilterSortedRecordingFile implements RecordedEventStream {
+
+  private final RecordedEventStream.Factory delegateStreamFactory;
+
+  public FilterSortedRecordingFile(RecordedEventStream.Factory delegateStreamFactory) {
+    this.delegateStreamFactory = delegateStreamFactory;
+  }
+
+  @Override
+  public Stream<RecordedEvent> open(Path path) {
+    return delegateStreamFactory
+        .get()
+        .open(path)
+        .filter(event -> RelevantEvents.EVENT_NAMES.contains(event.getEventType().getName()))
+        .sorted(Comparator.comparing(RecordedEvent::getStartTime));
+  }
+}
---
+++ profiler/src/main/java/com/splunk/opentelemetry/profiler/events/RelevantEvents.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.splunk.opentelemetry.profiler.events;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RelevantEvents {
+  public static Set<String> EVENT_NAMES =
+      Collections.unmodifiableSet(
+          new HashSet<>(Arrays.asList("jdk.ThreadDump", "otel.ContextAttached")));
+}

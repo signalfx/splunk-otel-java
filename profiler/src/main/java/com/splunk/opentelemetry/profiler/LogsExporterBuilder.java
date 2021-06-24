5c44eaf97aa523bc3dca7e333add8595d24d2e36
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2021-06-23 17:04:07 -0700

    allow logs ingest endpoint to be configured (#315)
    * allow logs ingest endpoint to be specified in config.
    
    * exclude local links from link checker
    
    * try again
    
    * factor out builder and add tests around endpoint override
    
    * code review cleanups

---
+++ profiler/src/main/java/com/splunk/opentelemetry/profiler/LogsExporterBuilder.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.splunk.opentelemetry.profiler;
+
+import static com.splunk.opentelemetry.profiler.Configuration.CONFIG_KEY_INGEST_URL;
+
+import com.splunk.opentelemetry.logs.InstrumentationLibraryLogsAdapter;
+import com.splunk.opentelemetry.logs.LogEntryAdapter;
+import com.splunk.opentelemetry.logs.LogsExporter;
+import com.splunk.opentelemetry.logs.OtlpLogsExporter;
+import com.splunk.opentelemetry.logs.OtlpLogsExporterBuilder;
+import com.splunk.opentelemetry.logs.ResourceLogsAdapter;
+import io.opentelemetry.instrumentation.api.config.Config;
+import io.opentelemetry.sdk.autoconfigure.OpenTelemetrySdkAutoConfiguration;
+import io.opentelemetry.sdk.resources.Resource;
+
+class LogsExporterBuilder {
+
+  private static final String OTEL_INSTRUMENTATION_NAME = "otel.profiling";
+  private static final String OTEL_INSTRUMENTATION_VERSION = "0.1.0";
+
+  static LogsExporter fromConfig(Config config) {
+    ResourceLogsAdapter adapter = buildResourceLogsAdapter();
+
+    OtlpLogsExporterBuilder builder =
+        OtlpLogsExporter.builder()
+            .setAdapter(adapter)
+            .addHeader("Extra-Content-Type", "otel-profiling-stacktraces");
+
+    String ingestUrl = config.getProperty(CONFIG_KEY_INGEST_URL);
+    if (ingestUrl != null) {
+      builder.setEndpoint(ingestUrl);
+    }
+    return builder.build();
+  }
+
+  private static ResourceLogsAdapter buildResourceLogsAdapter() {
+    Resource resource = OpenTelemetrySdkAutoConfiguration.getResource();
+    LogEntryAdapter logEntryAdapter = new LogEntryAdapter();
+    InstrumentationLibraryLogsAdapter instLibraryLogsAdapter =
+        InstrumentationLibraryLogsAdapter.builder()
+            .logEntryAdapter(logEntryAdapter)
+            .instrumentationName(OTEL_INSTRUMENTATION_NAME)
+            .instrumentationVersion(OTEL_INSTRUMENTATION_VERSION)
+            .build();
+    return new ResourceLogsAdapter(instLibraryLogsAdapter, resource);
+  }
+}

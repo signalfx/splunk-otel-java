89dea67fdcdbd6e5587402e5e9e22b09c410c497
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-06-30 19:16:32 +0200

    Introduce gradle pluginManagement (#328)

---
+++ settings.gradle.kts
@@ -0,0 +1,49 @@
+pluginManagement {
+  plugins {
+    id("com.bmuschko.docker-remote-api") version "7.1.0"
+    id("com.diffplug.spotless") version "5.14.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("nebula.release") version "15.3.1"
+    id("org.gradle.test-retry") version "1.3.1"
+  }
+}
+
+plugins {
+  id("com.gradle.enterprise") version "3.4.1"
+}
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
+}
+
+rootProject.name = "splunk-otel-java"
+include("agent",
+    "bootstrap",
+    "custom",
+    "instrumentation",
+    "instrumentation:common",
+    "instrumentation:commons-dbcp2",
+    "instrumentation:compile-stub",
+    "instrumentation:glassfish",
+    "instrumentation:jetty",
+    "instrumentation:jvm-metrics",
+    "instrumentation:khttp",
+    "instrumentation:liberty",
+    "instrumentation:netty-3.8",
+    "instrumentation:netty-4.0",
+    "instrumentation:servlet",
+    "instrumentation:servlet-3-testing",
+    "instrumentation:tomcat",
+    "instrumentation:tomee",
+    "instrumentation:weblogic",
+    "instrumentation:wildfly",
+    "matrix",
+    "profiler",
+    "smoke-tests",
+    "testing:agent-for-testing",
+    "testing:agent-test-extension",
+    "testing:profiler-tests",
+    "testing:common")

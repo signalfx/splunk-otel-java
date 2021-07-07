3db6b6393d3fd04d56fefaa5da5dd43b45a62c7a
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-07-07 11:33:36 +0200

    Clean up armeria-shaded-for-testing

--- instrumentation/khttp/build.gradle.kts
+++ instrumentation/khttp/build.gradle.kts
@@ -9,13 +9,10 @@ repositories {
   }
 }
 
-val otelInstrumentationAlphaVersion: String by extra
-
 dependencies {
   compileOnly("com.github.jkcclemens:khttp:0.1.0")
 
   testImplementation("com.github.jkcclemens:khttp:0.1.0")
 
   testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
-  testRuntimeOnly("io.opentelemetry.javaagent:opentelemetry-armeria-shaded-for-testing:${otelInstrumentationAlphaVersion}:all")
 }

6f17e70845bf624eae4c7cca1ed1002960186fde
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-07-13 16:47:20 +0200

    spotless

--- custom/src/test/java/com/splunk/opentelemetry/ServiceNameCheckerTest.java
+++ custom/src/test/java/com/splunk/opentelemetry/ServiceNameCheckerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.splunk.opentelemetry;
 
 import static org.mockito.ArgumentMatchers.anyString;
@@ -14,8 +30,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class ServiceNameCheckerTest {
-  @Mock
-  Consumer<String> logWarn;
+  @Mock Consumer<String> logWarn;
 
   @Test
   void shouldLogWarnWhenNeitherServiceNameNorResourceAttributeIsConfigured() {
@@ -34,9 +49,7 @@ class ServiceNameCheckerTest {
   @Test
   void shouldNotLogWarnWhenServiceNameIsConfigured() {
     // given
-    var config = Config.newBuilder()
-        .readProperties(Map.of("otel.service.name", "test"))
-        .build();
+    var config = Config.newBuilder().readProperties(Map.of("otel.service.name", "test")).build();
 
     var underTest = new ServiceNameChecker(logWarn);
 
@@ -50,9 +63,10 @@ class ServiceNameCheckerTest {
   @Test
   void shouldNotLogWarnWhenResourceAttributeIsConfigured() {
     // given
-    var config = Config.newBuilder()
-        .readProperties(Map.of("otel.resource.attributes", "service.name=test"))
-        .build();
+    var config =
+        Config.newBuilder()
+            .readProperties(Map.of("otel.resource.attributes", "service.name=test"))
+            .build();
 
     var underTest = new ServiceNameChecker(logWarn);
 

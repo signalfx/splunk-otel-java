3e878f0fcc4b1ff90a53b7c1435753e863472267
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-07-13 18:09:03 +0200

    Support OTEL_SERVICE_NAME (#353)
    * Support OTEL_SERVICE_NAME
    
    * spotless

--- custom/src/main/java/com/splunk/opentelemetry/ServiceNameChecker.java
+++ custom/src/main/java/com/splunk/opentelemetry/ServiceNameChecker.java
@@ -21,6 +21,7 @@ import io.opentelemetry.instrumentation.api.config.Config;
 import io.opentelemetry.javaagent.extension.AgentListener;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import java.util.Map;
+import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,15 +29,38 @@ import org.slf4j.LoggerFactory;
 public class ServiceNameChecker implements AgentListener {
   private static final Logger log = LoggerFactory.getLogger(ServiceNameChecker.class);
 
+  private final Consumer<String> logWarn;
+
+  public ServiceNameChecker() {
+    this(log::warn);
+  }
+
+  // visible for tests
+  ServiceNameChecker(Consumer<String> logWarn) {
+    this.logWarn = logWarn;
+  }
+
   @Override
   public void beforeAgent(Config config) {
-    Map<String, String> resourceAttributes = config.getMapProperty("otel.resource.attributes");
-    if (!resourceAttributes.containsKey(ResourceAttributes.SERVICE_NAME.getKey())) {
-      log.warn(
+    if (serviceNameNotConfigured(config)) {
+      logWarn.accept(
           "Resource attribute 'service.name' is not set: your service is unnamed and will be difficult to identify."
               + " Please Set your service name using the 'OTEL_RESOURCE_ATTRIBUTES' environment variable"
               + " or the 'otel.resource.attributes' system property."
               + " E.g. 'export OTEL_RESOURCE_ATTRIBUTES=\"service.name=<YOUR_SERVICE_NAME_HERE>\"'");
     }
+  }
+
+  // make sure this listener is one of the first things run by the agent
+  @Override
+  public int order() {
+    return -100;
+  }
+
+  private static boolean serviceNameNotConfigured(Config config) {
+    String serviceName = config.getProperty("otel.service.name");
+    Map<String, String> resourceAttributes = config.getMapProperty("otel.resource.attributes");
+    return serviceName == null
+        && !resourceAttributes.containsKey(ResourceAttributes.SERVICE_NAME.getKey());
   }
 }
---
+++ custom/src/test/java/com/splunk/opentelemetry/ServiceNameCheckerTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.splunk.opentelemetry;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import io.opentelemetry.instrumentation.api.config.Config;
+import java.util.Map;
+import java.util.function.Consumer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ServiceNameCheckerTest {
+  @Mock Consumer<String> logWarn;
+
+  @Test
+  void shouldLogWarnWhenNeitherServiceNameNorResourceAttributeIsConfigured() {
+    // given
+    var config = Config.newBuilder().build();
+
+    var underTest = new ServiceNameChecker(logWarn);
+
+    // when
+    underTest.beforeAgent(config);
+
+    // then
+    verify(logWarn).accept(anyString());
+  }
+
+  @Test
+  void shouldNotLogWarnWhenServiceNameIsConfigured() {
+    // given
+    var config = Config.newBuilder().readProperties(Map.of("otel.service.name", "test")).build();
+
+    var underTest = new ServiceNameChecker(logWarn);
+
+    // when
+    underTest.beforeAgent(config);
+
+    // then
+    verifyNoInteractions(logWarn);
+  }
+
+  @Test
+  void shouldNotLogWarnWhenResourceAttributeIsConfigured() {
+    // given
+    var config =
+        Config.newBuilder()
+            .readProperties(Map.of("otel.resource.attributes", "service.name=test"))
+            .build();
+
+    var underTest = new ServiceNameChecker(logWarn);
+
+    // when
+    underTest.beforeAgent(config);
+
+    // then
+    verifyNoInteractions(logWarn);
+  }
+}

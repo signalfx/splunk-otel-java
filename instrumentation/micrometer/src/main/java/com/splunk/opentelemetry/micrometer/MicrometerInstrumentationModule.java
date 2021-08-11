37f6ebd25c89c62c490352ef71cc2e373c72a02b
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-08-11 15:58:21 +0200

    spotless

--- instrumentation/micrometer/src/main/java/com/splunk/opentelemetry/micrometer/MicrometerInstrumentationModule.java
+++ instrumentation/micrometer/src/main/java/com/splunk/opentelemetry/micrometer/MicrometerInstrumentationModule.java
@@ -22,7 +22,6 @@ import com.google.auto.service.AutoService;
 import io.micrometer.core.instrument.Metrics;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
-import java.util.Arrays;
 import java.util.List;
 
 @AutoService(InstrumentationModule.class)

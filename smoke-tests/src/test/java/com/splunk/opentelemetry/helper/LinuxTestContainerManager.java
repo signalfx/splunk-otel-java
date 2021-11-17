d2c2a42cd90333e2e5c350631bd6786e8124f271
Lauri Tulmin <ltulmin@splunk.com>
2021-11-17 16:52:49 +0200

    Switch back to collector latest (#539)

--- smoke-tests/src/test/java/com/splunk/opentelemetry/helper/LinuxTestContainerManager.java
+++ smoke-tests/src/test/java/com/splunk/opentelemetry/helper/LinuxTestContainerManager.java
@@ -51,7 +51,7 @@ public class LinuxTestContainerManager extends AbstractTestContainerManager {
     backend.start();
 
     collector =
-        new GenericContainer<>(DockerImageName.parse("otel/opentelemetry-collector-contrib:0.38.0"))
+        new GenericContainer<>(DockerImageName.parse("otel/opentelemetry-collector-contrib:latest"))
             .dependsOn(backend)
             .withNetwork(network)
             .withNetworkAliases(COLLECTOR_ALIAS)

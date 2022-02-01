10a487d20b4448308033b5a22c4a9d33322177ec
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-02-01 07:11:43 +0100

    Accept gradle scans ToS only on CI (#651)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,10 +13,12 @@ plugins {
   id("com.gradle.enterprise") version "3.8.1"
 }
 
-gradleEnterprise {
-  buildScan {
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
+if (System.getenv("CI") != null) {
+  gradleEnterprise {
+    buildScan {
+      termsOfServiceUrl = "https://gradle.com/terms-of-service"
+      termsOfServiceAgree = "yes"
+    }
   }
 }
 

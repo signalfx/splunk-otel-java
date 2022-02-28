3682b9dd704bdecdccee8d3126b63a34da176398
Lauri Tulmin <ltulmin@splunk.com>
2022-02-28 15:19:01 +0200

    Add micrometer heap pressure metrics

--- instrumentation/jvm-metrics/src/main/java/com/splunk/opentelemetry/instrumentation/jvmmetrics/JvmMetricsInstaller.java
+++ instrumentation/jvm-metrics/src/main/java/com/splunk/opentelemetry/instrumentation/jvmmetrics/JvmMetricsInstaller.java
@@ -23,6 +23,7 @@ import com.google.auto.service.AutoService;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmHeapPressureMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import io.opentelemetry.instrumentation.api.config.Config;
@@ -41,6 +42,7 @@ public class JvmMetricsInstaller implements AgentListener {
 
     new ClassLoaderMetrics().bindTo(Metrics.globalRegistry);
     new JvmGcMetrics().bindTo(Metrics.globalRegistry);
+    new JvmHeapPressureMetrics().bindTo(Metrics.globalRegistry);
     new JvmMemoryMetrics().bindTo(Metrics.globalRegistry);
     new JvmThreadMetrics().bindTo(Metrics.globalRegistry);
 

8e76781a7563250ae61235036d1daca16374194b
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-03-08 08:46:05 +0100

    spotless

--- profiler/src/test/java/com/splunk/opentelemetry/profiler/LogDataCreatorTest.java
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/LogDataCreatorTest.java
@@ -16,7 +16,6 @@
 
 package com.splunk.opentelemetry.profiler;
 
-import static com.splunk.opentelemetry.profiler.LogDataCreator.PROFILING_SOURCE;
 import static com.splunk.opentelemetry.profiler.LogExporterBuilder.INSTRUMENTATION_LIBRARY_INFO;
 import static com.splunk.opentelemetry.profiler.ProfilingSemanticAttributes.SOURCE_EVENT_NAME;
 import static com.splunk.opentelemetry.profiler.ProfilingSemanticAttributes.SOURCE_EVENT_PERIOD;

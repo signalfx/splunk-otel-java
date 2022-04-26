869f3645c5d22a672a7e9075049559daa86837fb
Ago Allikmaa <ago.allikmaa@eesti.ee>
2022-04-26 16:56:39 +0100

    Automate overhead tests (and post-release PR) (#747) (#749)
    (cherry picked from commit 8d76e5e3755c92e3d51c9fbc451debd73f17d1df)
    
    Co-authored-by: Mateusz Rzeszutek <mrzeszutek@splunk.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,10 +23,12 @@ fi
 release_tag="$1"
 # without the starting 'v'
 release_version=$(echo "$release_tag" | cut -c2-)
-# v1 from v1.2.3
-major_version=$(echo "$release_tag" | awk -F'.' '{print $1}')
+# 1 from 1.2.3
+major_version=$(echo "$release_version" | awk -F'.' '{print $1}')
+minor_version=$(echo "$release_version" | awk -F'.' '{print $2}')
+patch_version=$(echo "$release_version" | awk -F'.' '{print $3}')
 
-import_gpg_keys() {
+setup_gpg() {
   echo ">>> Setting GnuPG configuration ..."
   mkdir -p ~/.gnupg
   chmod 700 ~/.gnupg
@@ -34,9 +36,13 @@ import_gpg_keys() {
 no-tty
 pinentry-mode loopback
 EOF
+}
+
+import_gpg_secret_key() {
+  local secret_key_contents="$1"
 
   echo ">>> Importing secret key ..."
-  echo "$GPG_SECRET_KEY" > seckey.gpg
+  echo "$secret_key_contents" > seckey.gpg
   if (gpg --batch --allow-secret-key-import --import seckey.gpg)
   then
     rm seckey.gpg
@@ -69,7 +75,7 @@ build_project() {
 sign_file() {
   local file="$1"
   echo "$GPG_PASSWORD" | \
-    gpg --batch --passphrase-fd 0 --armor --detach-sign "$file"
+    gpg --batch --passphrase-fd 0 --armor --default-key="$GPG_KEY_ID" --detach-sign "$file"
 }
 
 create_gh_release() {
@@ -81,25 +87,102 @@ create_gh_release() {
 }
 
 build_docker_image() {
+  echo ">>> Building the operator docker image ..."
   docker build -t splunk-otel-instrumentation-java .
   docker tag splunk-otel-instrumentation-java quay.io/signalfx/splunk-otel-instrumentation-java:$release_tag
-  docker tag splunk-otel-instrumentation-java quay.io/signalfx/splunk-otel-instrumentation-java:$major_version
+  docker tag splunk-otel-instrumentation-java quay.io/signalfx/splunk-otel-instrumentation-java:v$major_version
   docker tag splunk-otel-instrumentation-java quay.io/signalfx/splunk-otel-instrumentation-java:latest
 }
 
-publish_docker_image() {
-  docker push quay.io/signalfx/splunk-otel-instrumentation-java:latest
-  docker push quay.io/signalfx/splunk-otel-instrumentation-java:$release_tag
-  docker push quay.io/signalfx/splunk-otel-instrumentation-java:$major_version
-}
-
 login_to_quay_io() {
+  echo ">>> Logging into quay.io ..."
   docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 }
 
-import_gpg_keys
+publish_docker_image() {
+  echo ">>> Publishing the operator docker image ..."
+  docker push quay.io/signalfx/splunk-otel-instrumentation-java:latest
+  docker push quay.io/signalfx/splunk-otel-instrumentation-java:$release_tag
+  docker push quay.io/signalfx/splunk-otel-instrumentation-java:v$major_version
+}
+
+# TODO: add it to o11y-gdi env vars
+GITHUB_BOT_GPG_KEY_ID="A2581E631864FD064"
+
+setup_git() {
+  git config --global user.name release-bot
+  git config --global user.email ssg-srv-gh-o11y-gdi@splunk.com
+  git config --global gpg.program gpg
+  git config --global user.signingKey "$GITHUB_BOT_GPG_KEY_ID"
+}
+
+create_post_release_pr() {
+  if [[ $patch_version != 0 ]]
+  then
+    echo ">>> Patch release detected, skipping the post-release.sh script ..."
+    return
+  fi
+
+  local repo="signalfx/splunk-otel-java"
+  local repo_url="https://srv-gh-o11y-gdi:${GITHUB_TOKEN}@github.com/${repo}.git"
+  local next_version="${major_version}.$((minor_version + 1)).0"
+  local post_release_branch="post-release-changes-$release_tag"
+
+  echo ">>> Cloning the splunk-otel-java repository ..."
+  git clone "$repo_url" javaagent-mirror
+
+  echo ">>> Applying the post-release.sh script and pushing changes ..."
+  cd javaagent-mirror
+  git checkout -b "$post_release_branch"
+  ./scripts/post-release.sh "$release_version" "$next_version"
+  git commit -S -am "$release_tag post release changes"
+  git push "$repo_url" "$post_release_branch"
+
+  echo ">>> Creating the $release_tag post release PR ..."
+  local message="[automated] $release_tag post release changes"
+  gh pr create \
+    --repo "$repo" \
+    --title "$message" \
+    --body "$message" \
+    --base main \
+    --head "$post_release_branch"
+}
+
+create_overhead_test_pr() {
+  local repo="signalfx/splunk-otel-java-overhead-test"
+  local repo_url="https://srv-gh-o11y-gdi:${GITHUB_TOKEN}@github.com/${repo}.git"
+  local update_version_branch="update-agent-version-$release_tag"
+
+  echo ">>> Cloning the splunk-otel-java-overhead-test repository ..."
+  git clone "$repo_url" overhead-test-mirror
+
+  echo ">>> Applying the update-version.sh script and pushing changes ..."
+  cd overhead-test-mirror
+  git checkout -b "$update_version_branch"
+  ./scripts/update-version.sh "$release_version"
+  git commit -S -am "Update agent version to $release_tag"
+  git push "$repo_url" "$update_version_branch"
+
+  echo ">>> Creating the agent version update PR ..."
+  local message="[automated] Update agent version to $release_tag"
+  gh pr create \
+    --repo "$repo" \
+    --title "$message" \
+    --body "$message" \
+    --base main \
+    --head "$update_version_branch"
+}
+
+setup_gpg
+import_gpg_secret_key "$GPG_SECRET_KEY"
 build_project
 create_gh_release
+
 build_docker_image
 login_to_quay_io
 publish_docker_image
+
+import_gpg_secret_key "$GITHUB_BOT_GPG_KEY"
+setup_git
+create_post_release_pr
+create_overhead_test_pr

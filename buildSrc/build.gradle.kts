529ff3a77d4357c332bbb188eb7bc856702a821b
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-05-18 10:48:41 +0200

    bump one plugin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   `kotlin-dsl`
 
   // When updating, update below in dependencies too
-  id("com.diffplug.spotless") version "6.3.0"
+  id("com.diffplug.spotless") version "6.4.2"
 }
 
 spotless {

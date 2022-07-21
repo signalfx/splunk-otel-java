19dd7c39c02cfcecfd53fe94a8ec3ce3755c8d69
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-07-21 18:25:27 +0200

    Don't use InstrumentationConfig in AgentListener, use ConfigProperties instead

--- instrumentation/jvm-metrics/src/main/java/com/splunk/opentelemetry/instrumentation/jvmmetrics/JvmMetricsInstaller.java
+++ instrumentation/jvm-metrics/src/main/java/com/splunk/opentelemetry/instrumentation/jvmmetrics/JvmMetricsInstaller.java
@@ -19,7 +19,6 @@ package com.splunk.opentelemetry.instrumentation.jvmmetrics;
 import static com.splunk.opentelemetry.SplunkConfiguration.METRICS_ENABLED_PROPERTY;
 import static com.splunk.opentelemetry.SplunkConfiguration.METRICS_IMPLEMENTATION;
 import static com.splunk.opentelemetry.SplunkConfiguration.PROFILER_MEMORY_ENABLED_PROPERTY;
-import static java.util.Collections.singleton;
 
 import com.google.auto.service.AutoService;
 import com.splunk.opentelemetry.instrumentation.jvmmetrics.micrometer.MicrometerAllocatedMemoryMetrics;
@@ -36,34 +35,31 @@ import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmHeapPressureMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-import io.opentelemetry.javaagent.bootstrap.internal.InstrumentationConfig;
 import io.opentelemetry.javaagent.extension.AgentListener;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 
 @AutoService(AgentListener.class)
 public class JvmMetricsInstaller implements AgentListener {
-  private static final String metricsImplementation =
-      InstrumentationConfig.get().getString(METRICS_IMPLEMENTATION);
-  private static final boolean useOtelMetrics = "opentelemetry".equals(metricsImplementation);
-  private static final boolean useMicrometerMetrics = "micrometer".equals(metricsImplementation);
 
   @Override
   public void afterAgent(AutoConfiguredOpenTelemetrySdk autoConfiguredOpenTelemetrySdk) {
     ConfigProperties config = autoConfiguredOpenTelemetrySdk.getConfig();
     boolean metricsEnabled = config.getBoolean(METRICS_ENABLED_PROPERTY, false);
-    if (!isInstrumentationEnabled(config, singleton("jvm-metrics-splunk"), metricsEnabled)) {
+    if (!config.getBoolean("otel.instrumentation.jvm-metrics.splunk.enabled", metricsEnabled)) {
       return;
     }
 
-    if (useMicrometerMetrics) {
+    String metricsImplementation = config.getString(METRICS_IMPLEMENTATION);
+
+    if (useMicrometerMetrics(metricsImplementation)) {
       new ClassLoaderMetrics().bindTo(Metrics.globalRegistry);
       new JvmGcMetrics().bindTo(Metrics.globalRegistry);
       new JvmHeapPressureMetrics().bindTo(Metrics.globalRegistry);
       new JvmMemoryMetrics().bindTo(Metrics.globalRegistry);
       new JvmThreadMetrics().bindTo(Metrics.globalRegistry);
     }
-    if (useOtelMetrics) {
+    if (useOtelMetrics(metricsImplementation)) {
       new OtelJvmGcMetrics().install();
       new OtelJvmHeapPressureMetrics().install();
       new OtelJvmMemoryMetrics().install();
@@ -73,32 +69,22 @@ public class JvmMetricsInstaller implements AgentListener {
     // Following metrics are experimental, we'll enable them only when memory profiling is enabled
     if (config.getBoolean(PROFILER_MEMORY_ENABLED_PROPERTY, false)
         || config.getBoolean("splunk.metrics.experimental.enabled", false)) {
-      if (useMicrometerMetrics) {
+      if (useMicrometerMetrics(metricsImplementation)) {
         new MicrometerAllocatedMemoryMetrics().bindTo(Metrics.globalRegistry);
         new MicrometerGcMemoryMetrics().bindTo(Metrics.globalRegistry);
       }
-      if (useOtelMetrics) {
+      if (useOtelMetrics(metricsImplementation)) {
         new OtelAllocatedMemoryMetrics().install();
         new OtelGcMemoryMetrics().install();
       }
     }
   }
 
-  private static boolean isInstrumentationEnabled(
-      ConfigProperties config, Iterable<String> instrumentationNames, boolean defaultEnabled) {
-    // If default is enabled, we want to enable individually,
-    // if default is disabled, we want to disable individually.
-    boolean anyEnabled = defaultEnabled;
-    for (String name : instrumentationNames) {
-      String propertyName = "otel.instrumentation." + name.replace('-', '.') + ".enabled";
-      boolean enabled = config.getBoolean(propertyName, defaultEnabled);
+  private static boolean useMicrometerMetrics(String metricsImplementation) {
+    return "micrometer".equalsIgnoreCase(metricsImplementation);
+  }
 
-      if (defaultEnabled) {
-        anyEnabled &= enabled;
-      } else {
-        anyEnabled |= enabled;
-      }
-    }
-    return anyEnabled;
+  private static boolean useOtelMetrics(String metricsImplementation) {
+    return "opentelemetry".equalsIgnoreCase(metricsImplementation);
   }
 }

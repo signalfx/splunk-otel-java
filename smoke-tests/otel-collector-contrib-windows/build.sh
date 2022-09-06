b842df7ebf8ab857dc21c65eee751aaa4ca6f9a7
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-09-06 15:45:43 +0200

    Use splunk-otel-collector windows image instead of a custom built one

--- smoke-tests/otel-collector-contrib-windows/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-EXTRA_TAG=$1
-IMAGE_NAME="ghcr.io/signalfx/splunk-otel-java/otel-collector-contrib-windows:$(date '+%Y%m%d').${EXTRA_TAG}"
-
-echo "Building image ${IMAGE_NAME} ..."
-
-docker build -t "$IMAGE_NAME" .
-docker push "$IMAGE_NAME"

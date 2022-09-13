0ec203b124dac8b8c5ca143b3a2f2e648aed8405
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-09-13 13:07:45 +0200

    Fix CI build (#913)
    * Fix CI build
    
    * spotless

--- instrumentation/khttp/src/main/java/com/splunk/opentelemetry/instrumentation/khttp/KHttpSingletons.java
+++ instrumentation/khttp/src/main/java/com/splunk/opentelemetry/instrumentation/khttp/KHttpSingletons.java
@@ -18,13 +18,13 @@ package com.splunk.opentelemetry.instrumentation.khttp;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
-import io.opentelemetry.instrumentation.api.instrumenter.PeerServiceAttributesExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpClientAttributesExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpClientAttributesGetter;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpClientMetrics;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpSpanNameExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpSpanStatusExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.net.NetClientAttributesExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.net.PeerServiceAttributesExtractor;
 import io.opentelemetry.javaagent.bootstrap.internal.CommonConfig;
 import khttp.responses.Response;
 

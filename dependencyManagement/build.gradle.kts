ce17d11729591aad4fb69fa3e583e589a8d2ee93
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2023-03-06 01:31:55 -0800

    update contrib version to 1.23.0-alpha (#1141)

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -6,7 +6,7 @@ val otelVersion = "1.23.1"
 val otelAlphaVersion = "1.23.1-alpha"
 val otelInstrumentationVersion = "1.24.0-SNAPSHOT"
 val otelInstrumentationAlphaVersion = "1.24.0-alpha-SNAPSHOT"
-val otelContribAlphaVersion = "1.22.0-alpha"
+val otelContribAlphaVersion = "1.23.0-alpha"
 
 val micrometerVersion = "1.10.4"
 val micrometerOldVersion = "1.3.20"

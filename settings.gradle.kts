4afe16cb79c9753f813abfb30e11ede54df028c0
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2023-03-08 16:18:06 +0100

    Remove test sets plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ pluginManagement {
     id("com.github.johnrengelman.shadow") version "8.1.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
     id("nebula.release") version "17.2.0"
-    id("org.unbroken-dome.test-sets") version "4.0.0"
   }
 }
 

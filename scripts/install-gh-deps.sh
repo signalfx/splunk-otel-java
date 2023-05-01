8b0bf9b575cd872dc4f17ed299c204a079288b31
Jason Plumb <jplumb@splunk.com>
2023-05-01 14:52:54 -0700

    after release, push a version update PR to the helm chart repo.

--- scripts/install-gh-deps.sh
+++ scripts/install-gh-deps.sh
@@ -6,5 +6,5 @@ set -e
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
-apt-get update
-apt-get -y install zip gh
+apt update
+apt install -y zip gh

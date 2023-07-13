f54eda1f016d7062169ffe04de5b72b2ca696772
Lauri Tulmin <ltulmin@splunk.com>
2023-07-13 17:50:35 +0300

    Change otel instrumentation agent dependency to 1.28.0 (#1360)

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 val otelVersion = "1.28.0"
 val otelAlphaVersion = otelVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
-val otelInstrumentationVersion = "1.28.0-SNAPSHOT"
+val otelInstrumentationVersion = "1.28.0"
 val otelInstrumentationAlphaVersion =  otelInstrumentationVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
 val otelContribAlphaVersion = "1.27.0-alpha"
 

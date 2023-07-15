8db4954b2e08f30e6c3faaf9c71c4842f5bc9463
Lauri Tulmin <ltulmin@splunk.com>
2023-07-15 13:27:41 +0300

    Change upstream agent to snapshot version (#1366)

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 val otelVersion = "1.28.0"
 val otelAlphaVersion = otelVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
-val otelInstrumentationVersion = "1.28.0"
+val otelInstrumentationVersion = "1.29.0-SNAPSHOT"
 val otelInstrumentationAlphaVersion =  otelInstrumentationVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
 val otelContribAlphaVersion = "1.27.0-alpha"
 

5d0957b534253517b64e71d5ea4082fd569bd6ce
Lauri Tulmin <ltulmin@splunk.com>
2023-12-15 11:56:02 +0200

    Also update the otel java version

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   `java-platform`
 }
 
-val otelVersion = "1.32.0"
+val otelVersion = "1.33.0"
 val otelAlphaVersion = otelVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
 val otelInstrumentationVersion = "1.33.0-SNAPSHOT"
 val otelInstrumentationAlphaVersion =  otelInstrumentationVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")

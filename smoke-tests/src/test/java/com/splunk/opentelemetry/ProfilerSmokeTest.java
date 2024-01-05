45b8d48344a60398004d69dc5e548c8c24322332
Lauri Tulmin <ltulmin@splunk.com>
2024-01-05 19:50:47 +0200

    fix tests

--- smoke-tests/src/test/java/com/splunk/opentelemetry/ProfilerSmokeTest.java
+++ smoke-tests/src/test/java/com/splunk/opentelemetry/ProfilerSmokeTest.java
@@ -281,7 +281,7 @@ public abstract class ProfilerSmokeTest {
                 "-Dsplunk.profiler.directory=/app/jfr",
                 "-Dsplunk.profiler.keep-files=true",
                 "-Dsplunk.profiler.call.stack.interval=1001",
-                "-Dsplunk.profiler.logs-endpoint=http://collector:4318",
+                "-Dsplunk.profiler.logs-endpoint=http://collector:4317",
                 // uncomment to enable exporting traces
                 // "-Dotel.exporter.otlp.endpoint=http://collector:4318",
                 "-jar",

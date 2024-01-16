00b85fb78494a1bca29898f413f86e8ceb172248
Lauri Tulmin <ltulmin@splunk.com>
2024-01-16 14:53:35 +0200

    Remove splunk.profiler.tlab.enabled option

--- profiler/src/test/java/com/splunk/opentelemetry/profiler/ConfigurationTest.java
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/ConfigurationTest.java
@@ -17,9 +17,7 @@
 package com.splunk.opentelemetry.profiler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -68,38 +66,5 @@ class ConfigurationTest {
     when(config.getString(Configuration.CONFIG_KEY_INGEST_URL, null)).thenReturn(null);
     String result = Configuration.getConfigUrl(config);
     assertNull(result);
-  }
-
-  @Test
-  void getTLABEnabled_override() {
-    ConfigProperties config = mock(ConfigProperties.class);
-    when(config.getBoolean(
-            Configuration.CONFIG_KEY_MEMORY_ENABLED, Configuration.DEFAULT_MEMORY_ENABLED))
-        .thenReturn(false);
-    when(config.getBoolean(Configuration.CONFIG_KEY_TLAB_ENABLED, false)).thenReturn(true);
-    boolean result = Configuration.getTLABEnabled(config);
-    assertTrue(result);
-  }
-
-  @Test
-  void getTLABEnabled_inheritedTrue() {
-    ConfigProperties config = mock(ConfigProperties.class);
-    when(config.getBoolean(
-            Configuration.CONFIG_KEY_MEMORY_ENABLED, Configuration.DEFAULT_MEMORY_ENABLED))
-        .thenReturn(true);
-    when(config.getBoolean(Configuration.CONFIG_KEY_TLAB_ENABLED, true)).thenReturn(true);
-    boolean result = Configuration.getTLABEnabled(config);
-    assertTrue(result);
-  }
-
-  @Test
-  void getTLABEnabled_inheritedFalse() {
-    ConfigProperties config = mock(ConfigProperties.class);
-    when(config.getBoolean(
-            Configuration.CONFIG_KEY_MEMORY_ENABLED, Configuration.DEFAULT_MEMORY_ENABLED))
-        .thenReturn(false);
-    when(config.getBoolean(Configuration.CONFIG_KEY_TLAB_ENABLED, false)).thenReturn(false);
-    boolean result = Configuration.getTLABEnabled(config);
-    assertFalse(result);
   }
 }

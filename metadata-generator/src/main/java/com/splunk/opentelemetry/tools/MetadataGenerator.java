0410b64520a2a771ca47e436c9cda37e736f095f
Lauri Tulmin <ltulmin@splunk.com>
2024-01-30 10:01:28 +0200

    Rename deprecated configuration property

--- metadata-generator/src/main/java/com/splunk/opentelemetry/tools/MetadataGenerator.java
+++ metadata-generator/src/main/java/com/splunk/opentelemetry/tools/MetadataGenerator.java
@@ -1498,7 +1498,7 @@ public class MetadataGenerator {
     // https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/main/instrumentation/kafka/README.md
     /*
     | `otel.instrumentation.kafka.experimental-span-attributes` | Boolean | `false` | Enable the capture of experimental span attributes.                                                                        |
-    | `otel.instrumentation.kafka.client-propagation.enabled`   | Boolean | `true`  | Enable context propagation for kafka message producer.                                                                     |
+    | `otel.instrumentation.kafka.producer-propagation.enabled` | Boolean | `true`  | Enable context propagation for kafka message producer.                                                                     |
     | `otel.instrumentation.kafka.metric-reporter.enabled`      | Boolean | `true`  | Enable kafka consumer and producer metrics. Deprecated, disable instrumentation with name `kafka-clients-metrics` instead. |
      */
     settings.add(

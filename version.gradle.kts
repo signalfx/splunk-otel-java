1cb94498e96b351b48c238017c30815629ad270b
Lauri Tulmin <ltulmin@splunk.com>
2024-02-22 08:17:04 +0200

    fix version

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 // do NOT update that variable manually - it is managed by the pre/post release scripts
-val distroVersion = "2.2.0-SNAPSHOT"
+val distroVersion = "2.2.0-alpha-SNAPSHOT"
 
 allprojects {
   version = distroVersion

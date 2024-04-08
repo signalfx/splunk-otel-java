c3f6cb2d19cc688cd6d06b8291ed5fb6aad4a26b
Lauri Tulmin <ltulmin@splunk.com>
2024-04-08 21:37:11 +0300

    Use a fork of khttp

--- instrumentation/khttp/build.gradle.kts
+++ instrumentation/khttp/build.gradle.kts
@@ -4,24 +4,18 @@ plugins {
   id("splunk.muzzle-conventions")
 }
 
-repositories {
-  maven {
-    url = uri("https://jitpack.io")
-  }
-}
-
 muzzle {
   pass {
-    group.set("com.github.jkcclemens")
+    group.set("org.danilopianini")
     module.set("khttp")
     versions.set("(,)")
   }
 }
 
 dependencies {
-  compileOnly("com.github.jkcclemens:khttp:0.1.0")
+  compileOnly("org.danilopianini:khttp:1.1.0")
 
-  testImplementation("com.github.jkcclemens:khttp:0.1.0")
+  testImplementation("org.danilopianini:khttp:1.1.0")
 
   testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }

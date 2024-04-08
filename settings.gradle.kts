063716908ffddb176d139f4e664b7d6acc04e0e0
Lauri Tulmin <ltulmin@splunk.com>
2024-04-08 10:42:11 +0300

    Delete khttp instrumentation from 1.32.x

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,6 @@ include(
     "instrumentation:hikaricp",
     "instrumentation:jetty",
     "instrumentation:jvm-metrics",
-    "instrumentation:khttp-0.1",
     "instrumentation:liberty",
     "instrumentation:micrometer-1.3",
     "instrumentation:micrometer-1.3-shaded-for-instrumenting",

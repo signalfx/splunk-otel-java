959b2860b1b96ebf5b792d6ae1403ed803d307f5
Jason Plumb <jplumb@splunk.com>
2024-05-29 14:10:24 -0700

    update to alpha

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 // do NOT update that variable manually - it is managed by the pre/post release scripts
-val distroVersion = "2.4.0"
+val distroVersion = "2.4.0-alpha"
 
 allprojects {
   version = distroVersion

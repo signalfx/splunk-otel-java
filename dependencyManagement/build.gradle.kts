65f9c794db85595a8d3b43546b031b193419f66d
Lauri Tulmin <ltulmin@splunk.com>
2024-06-20 20:08:08 +0300

    Update otel agent to 2.5.0 (#1905)

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 
 val otelVersion = "1.39.0"
 val otelAlphaVersion = otelVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
-val otelInstrumentationVersion = "2.5.0-SNAPSHOT"
+val otelInstrumentationVersion = "2.5.0"
 val otelInstrumentationAlphaVersion =  otelInstrumentationVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
-val otelContribAlphaVersion = "1.35.0-alpha"
+val otelContribAlphaVersion = "1.36.0-alpha"
 
 val autoValueVersion = "1.11.0"
 val dockerJavaVersion = "3.3.6"

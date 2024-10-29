93b8e81bdc2482ad7526f639c93a81b1a2b225a8
Lauri Tulmin <ltulmin@splunk.com>
2024-10-29 09:12:39 +0200

    update gradle plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
   implementation(gradleApi())
 
   implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
-  implementation("io.opentelemetry.instrumentation:gradle-plugins:2.6.0-alpha")
+  implementation("io.opentelemetry.instrumentation:gradle-plugins:2.9.0-alpha")
 
   // keep these versions in sync with settings.gradle.kts
   implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.4")

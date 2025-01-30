e4ecdeac48f5d7bb0e09cd2e643fae1100927763
thomasduncan <tduncan@splunk.com>
2025-01-30 11:32:53 -0800

    Add initial implementation of the TraceRegistry interface.

---
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/DefaultTraceRegistryTest.java
@@ -0,0 +1,54 @@
+package com.splunk.opentelemetry.profiler.snapshot;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.opentelemetry.api.trace.SpanContext;
+import io.opentelemetry.api.trace.SpanId;
+import io.opentelemetry.api.trace.TraceFlags;
+import io.opentelemetry.api.trace.TraceId;
+import io.opentelemetry.api.trace.TraceState;
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+
+class DefaultTraceRegistryTest {
+  private static final Random RANDOM = new Random();
+
+  private final DefaultTraceRegistry registry = new DefaultTraceRegistry();
+
+  @Test
+  void registerTrace() {
+    var spanContext = newSpanContext();
+    registry.register(spanContext);
+    assertTrue(registry.isRegistered(spanContext));
+  }
+
+  @Test
+  void unregisteredTracesAreNotRegisteredForProfiling() {
+    var spanContext = newSpanContext();
+    assertFalse(registry.isRegistered(spanContext));
+  }
+
+  @Test
+  void unregisterTraceForProfiling() {
+    var spanContext = newSpanContext();
+
+    registry.register(spanContext);
+    registry.unregister(spanContext);
+
+    assertFalse(registry.isRegistered(spanContext));
+  }
+
+  private SpanContext newSpanContext() {
+    return newSpanContext(randomTraceId());
+  }
+
+  private SpanContext newSpanContext(String traceId) {
+    var spanId = SpanId.fromLong(RANDOM.nextLong());
+    return SpanContext.create(traceId, spanId, TraceFlags.getDefault(), TraceState.getDefault());
+  }
+
+  private String randomTraceId() {
+    return TraceId.fromLongs(RANDOM.nextLong(), RANDOM.nextLong());
+  }
+}

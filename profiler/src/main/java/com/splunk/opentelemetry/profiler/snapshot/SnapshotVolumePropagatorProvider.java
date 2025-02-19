3b34a8805b533e4f5b8af80a166cfbb073d90e42
thomasduncan <tduncan@splunk.com>
2025-02-19 10:37:13 -0800

    Add SnapshotVolumePropagatorProvider.

---
+++ profiler/src/main/java/com/splunk/opentelemetry/profiler/snapshot/SnapshotVolumePropagatorProvider.java
@@ -0,0 +1,19 @@
+package com.splunk.opentelemetry.profiler.snapshot;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigurablePropagatorProvider;
+
+@AutoService(ConfigurablePropagatorProvider.class)
+public class SnapshotVolumePropagatorProvider implements ConfigurablePropagatorProvider {
+  @Override
+  public TextMapPropagator getPropagator(ConfigProperties config) {
+    return new SnapshotProfilingSignalPropagator();
+  }
+
+  @Override
+  public String getName() {
+    return "splunk-snapshot";
+  }
+}
---
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/SnapshotVolumePropagatorProviderTest.java
@@ -0,0 +1,23 @@
+package com.splunk.opentelemetry.profiler.snapshot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+import io.opentelemetry.sdk.autoconfigure.spi.internal.DefaultConfigProperties;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+
+class SnapshotVolumePropagatorProviderTest {
+  private final SnapshotVolumePropagatorProvider provider = new SnapshotVolumePropagatorProvider();
+
+  @Test
+  void provideSnapshotVolumePropagator() {
+    var propagator = provider.getPropagator(DefaultConfigProperties.create(Collections.emptyMap()));
+    assertInstanceOf(SnapshotProfilingSignalPropagator.class, propagator);
+  }
+
+  @Test
+  void name() {
+    assertEquals("splunk-snapshot", provider.getName());
+  }
+}

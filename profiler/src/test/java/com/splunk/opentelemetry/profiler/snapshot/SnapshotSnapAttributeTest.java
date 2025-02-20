168b24f13574d3bfdad927a8e591a439e6b194e0
thomasduncan <tduncan@splunk.com>
2025-02-19 16:34:37 -0800

    Apply spotless code formatting.

--- profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/SnapshotSnapAttributeTest.java
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/SnapshotSnapAttributeTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.splunk.opentelemetry.profiler.snapshot;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,7 +29,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 
 class SnapshotSnapAttributeTest {
   private final TogglableTraceRegistry registry = new TogglableTraceRegistry();
-  private final SnapshotProfilingSdkCustomizer customizer = new SnapshotProfilingSdkCustomizer(registry);
+  private final SnapshotProfilingSdkCustomizer customizer =
+      new SnapshotProfilingSdkCustomizer(registry);
 
   @RegisterExtension
   public final OpenTelemetrySdkExtension s =
@@ -46,7 +63,8 @@ class SnapshotSnapAttributeTest {
   @SpanKinds.Entry
   void addSnapshotSpanAttributeToAllEntrySpans(SpanKind kind, Tracer tracer) {
     try (var root = tracer.spanBuilder("root").setSpanKind(kind).startSpan().makeCurrent()) {
-      try (var client = tracer.spanBuilder("client").setSpanKind(SpanKind.CLIENT).startSpan().makeCurrent()) {
+      try (var client =
+          tracer.spanBuilder("client").setSpanKind(SpanKind.CLIENT).startSpan().makeCurrent()) {
         var span = (ReadWriteSpan) tracer.spanBuilder("root").setSpanKind(kind).startSpan();
         try (var ignored = span.makeCurrent()) {
           var attribute = span.getAttribute(AttributeKey.booleanKey("splunk.snapshot.profiling"));
@@ -58,7 +76,8 @@ class SnapshotSnapAttributeTest {
 
   @ParameterizedTest
   @SpanKinds.Entry
-  void doNotAddSnapshotSpanAttributeWhenTraceIsNotRegisteredForSnapshotting(SpanKind kind, Tracer tracer) {
+  void doNotAddSnapshotSpanAttributeWhenTraceIsNotRegisteredForSnapshotting(
+      SpanKind kind, Tracer tracer) {
     registry.toggle(State.OFF);
 
     var span = (ReadWriteSpan) tracer.spanBuilder("root").setSpanKind(kind).startSpan();

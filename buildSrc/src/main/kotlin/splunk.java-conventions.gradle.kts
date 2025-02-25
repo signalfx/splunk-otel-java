3582102a4dc8e42cf03db32b9c9578a909f1c27e
Lauri Tulmin <ltulmin@splunk.com>
2025-02-25 10:41:47 +0200

    fix dependencies

--- buildSrc/src/main/kotlin/splunk.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/splunk.java-conventions.gradle.kts
@@ -34,8 +34,8 @@ dependencies {
   add("testImplementation", "org.mockito:mockito-core")
   add("testImplementation", "org.mockito:mockito-junit-jupiter")
   add("testImplementation", "org.junit.jupiter:junit-jupiter-api")
-  add("testImplementation", "org.junit.jupiter:junit-jupiter-params")
   add("testRuntimeOnly", "org.junit.jupiter:junit-jupiter-engine")
+  add("testRuntimeOnly", "org.junit.platform:junit-platform-launcher")
   add("testRuntimeOnly", "org.slf4j:slf4j-api")
 }
 

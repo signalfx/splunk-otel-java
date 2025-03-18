e9074559fe83938846f996ee9bc6d1dfb9f28b3c
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2025-03-18 13:53:03 -0700

    Add ability to add additional context for appd integrations. (#2198)
    * add ability to add additional context for appd integrations.
    
    * add span processor explicitly to tracer builder, instead of composing.
    
    * fix autoconfigured components
    
    * move constants closer to usage
    
    * don't inject if "none" propagator selected.
    
    * spotless
    
    * address code review comments
    
    * move constants out of single package file

---
+++ custom/src/main/java/com/splunk/opentelemetry/appd/AppdBonusPropagatorProvider.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.splunk.opentelemetry.appd;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigurablePropagatorProvider;
+
+@AutoService(ConfigurablePropagatorProvider.class)
+public class AppdBonusPropagatorProvider implements ConfigurablePropagatorProvider {
+
+  @Override
+  public TextMapPropagator getPropagator(ConfigProperties config) {
+    return AppdBonusPropagator.getInstance();
+  }
+
+  @Override
+  public String getName() {
+    return AppdBonusPropagator.NAME;
+  }
+}

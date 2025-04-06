c222e4632239ad34edfa7c49346b3df86b48782b
Lauri Tulmin <ltulmin@splunk.com>
2025-04-06 14:05:51 +0300

    Update otel sdk to 1.49.0

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   `java-platform`
 }
 
-val otelVersion = "1.48.0"
+val otelVersion = "1.49.0"
 val otelAlphaVersion = otelVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")
 val otelInstrumentationVersion = "2.15.0-SNAPSHOT"
 val otelInstrumentationAlphaVersion =  otelInstrumentationVersion.replaceFirst("(-SNAPSHOT)?$".toRegex(), "-alpha$1")

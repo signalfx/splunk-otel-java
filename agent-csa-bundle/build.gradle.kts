8d14580afed21fe662ed1c329ab37fe350c2b4d3
Jason Plumb <jplumb@splunk.com>
2025-04-23 13:45:17 -0700

    restore publishing and clean up boneyard

--- agent-csa-bundle/build.gradle.kts
+++ agent-csa-bundle/build.gradle.kts
@@ -34,12 +34,6 @@ repositories {
 
 dependencies {
   runtimeOnly(project(":agent", configuration = "shadow"))
-//  runtimeOnly("signalfx:csa-releases:${csaVersion}") {
-//    artifact {
-//      name = "oss-agent-mtagent-extension-deployment"
-//      extension = "jar"
-//    }
-//  }
   csaReleases("signalfx:csa-releases:${csaVersion}") {
     artifact {
       name = "oss-agent-mtagent-extension-deployment"
@@ -82,7 +76,7 @@ tasks {
     from(copyCsaJar)
   }
 
-  // Copy service file
+  // Copy service file so path on disk matches path in jar
   val copyServiceFile by registering(Copy::class){
     dependsOn(extractExtensionClasses)
     from("build/ext-exploded/META-INF/services/")
@@ -134,119 +128,54 @@ tasks {
     dependsOn(shadowJar)
   }
 
-}
+  publishing {
+    publications {
+      register<MavenPublication>("maven") {
+        artifactId = "splunk-otel-javaagent-csa"
+        groupId = "com.splunk"
+        version = project.version.toString()
 
-//
-//
-//val upstreamSecureApp: Configuration by configurations.creating {
-//  isCanBeResolved = true
-//  isCanBeConsumed = false
-//}
-//val splunkJavaAgent: Configuration by configurations.creating {
-//  isCanBeResolved = true
-//  isCanBeConsumed = false
-//}
-//
-//dependencies {
-//  implementation(project(":agent"))
-//  implementation("signalfx:csa-releases:${csaVersion}"){
-//    artifact {
-//      name = "oss-agent-mtagent-extension-deployment"
-//      extension = "jar"
-//    }
-//  }
-//}
-//
-//tasks {
-//
-//  jar {
-//    enabled = true
-//  }
-//
-//  shadowJar {
-//    configurations = listOf(splunkJavaAgent, upstreamSecureApp)
-//    dependsOn(splunkJavaAgent, upstreamSecureApp)
-////    from(...)
-////    dependencies {
-////      include(dependency(project(":agent")))
-////    }
-//    manifest {
-//      attributes(
-//        mapOf(
-//          "Main-Class" to "io.opentelemetry.javaagent.OpenTelemetryAgent",
-//          "Agent-Class" to "com.splunk.opentelemetry.javaagent.SplunkAgent",
-//          "Premain-Class" to "com.splunk.opentelemetry.javaagent.SplunkAgent",
-//          "Can-Redefine-Classes" to "true",
-//          "Can-Retransform-Classes" to "true",
-//          "Implementation-Vendor" to "Splunk",
-//          "Implementation-Version" to "splunk-${project.version}-otel-$otelInstrumentationVersion",
-//          "Cisco-Secure-Application-Version" to csaVersion,
-//        ))
-//    }
-//  }
-//
-////  val bundleSecureAppShadowJar by registering(Jar::class) {
-////    archiveClassifier.set("")
-////
-////    from(zipTree(shadowJar.get().archiveFile))
-////
-////    manifest {
-////      attributes(shadowJar.get().manifest.attributes)
-////    }
-////  }
-//
-//  assemble {
-//    dependsOn(shadowJar)
-//  }
-//
-//  publishing {
-//    publications {
-//      register<MavenPublication>("maven") {
-//        artifactId = "splunk-otel-javaagent-csa"
-//        groupId = "com.splunk"
-//        version = project.version.toString()
-//
-//        artifact(shadowJar)
-//
-//        pom {
-//          name.set("splunk-otel-java with Cisco SecureApp bundle")
-//          description.set("A distribution of the OpenTelemetry Instrumentation for Java which includes a bundled version of the Cisco SecureApp")
-//          url.set("https://github.com/signalfx/splunk-otel-java")
-//          packaging = "jar"
-//
-//          licenses {
-//            license {
-//              name.set("The Apache License, Version 2.0")
-//              url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//            }
-//          }
-//
-//          developers {
-//            developer {
-//              id.set("splunk")
-//              name.set("Splunk Instrumentation Authors")
-//              email.set("support+java@signalfx.com")
-//              organization.set("Splunk")
-//              organizationUrl.set("https://www.splunk.com")
-//            }
-//          }
-//
-//          scm {
-//            connection.set("https://github.com/signalfx/splunk-otel-java.git")
-//            developerConnection.set("https://github.com/signalfx/splunk-otel-java.git")
-//            url.set("https://github.com/signalfx/splunk-otel-java")
-//          }
-//        }
-//      }
-//    }
-//  }
-//
-//  val gpgSecretKey = System.getenv("GPG_SECRET_KEY")
-//  val gpgPassword = System.getenv("GPG_PASSWORD")
-//  if (gpgSecretKey != null && gpgPassword != null) {
-//    signing {
-//      useInMemoryPgpKeys(gpgSecretKey, gpgPassword)
-//      sign(publishing.publications["maven"])
-//    }
-//  }
-//}
+        artifact(shadowJar)
+
+        pom {
+          name.set("splunk-otel-java with Cisco SecureApp bundle")
+          description.set("A distribution of the OpenTelemetry Instrumentation for Java which includes a bundled version of the Cisco SecureApp")
+          url.set("https://github.com/signalfx/splunk-otel-java")
+          packaging = "jar"
+
+          licenses {
+            license {
+              name.set("The Apache License, Version 2.0")
+              url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+          }
+
+          developers {
+            developer {
+              id.set("splunk")
+              name.set("Splunk Instrumentation Authors")
+              email.set("support+java@signalfx.com")
+              organization.set("Splunk")
+              organizationUrl.set("https://www.splunk.com")
+            }
+          }
+
+          scm {
+            connection.set("https://github.com/signalfx/splunk-otel-java.git")
+            developerConnection.set("https://github.com/signalfx/splunk-otel-java.git")
+            url.set("https://github.com/signalfx/splunk-otel-java")
+          }
+        }
+      }
+    }
+  }
+
+  val gpgSecretKey = System.getenv("GPG_SECRET_KEY")
+  val gpgPassword = System.getenv("GPG_PASSWORD")
+  if (gpgSecretKey != null && gpgPassword != null) {
+    signing {
+      useInMemoryPgpKeys(gpgSecretKey, gpgPassword)
+      sign(publishing.publications["maven"])
+    }
+  }
+}

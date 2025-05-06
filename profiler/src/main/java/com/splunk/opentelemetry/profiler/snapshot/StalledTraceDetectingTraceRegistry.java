60dc92e7edd2165a22b84754454cfa5132ac5836
thomasduncan <tduncan@splunk.com>
2025-05-06 16:05:58 -0700

    Add basic TraceRegistry implementation for unregistering stalled traces.

---
+++ profiler/src/main/java/com/splunk/opentelemetry/profiler/snapshot/StalledTraceDetectingTraceRegistry.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.splunk.opentelemetry.profiler.snapshot;
+
+import com.splunk.opentelemetry.profiler.util.HelpfulExecutors;
+import io.opentelemetry.api.trace.SpanContext;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+class StalledTraceDetectingTraceRegistry implements TraceRegistry, AutoCloseable {
+  private final ScheduledExecutorService scheduler =
+      HelpfulExecutors.newSingleThreadedScheduledExecutor("stalled-trace-detector");
+  private final Map<String, RegistrationContext> traceIds = new ConcurrentHashMap<>();
+  private final TraceRegistry delegate;
+
+  StalledTraceDetectingTraceRegistry(TraceRegistry delegate) {
+    this(delegate, Duration.ofMillis(10));
+  }
+
+  public StalledTraceDetectingTraceRegistry(TraceRegistry delegate, Duration stalledTimeLimit) {
+    this.delegate = delegate;
+
+    scheduler.scheduleAtFixedRate(
+        removeStalledTraces(stalledTimeLimit),
+        stalledTimeLimit.toMillis() / 2,
+        stalledTimeLimit.toMillis() / 2,
+        TimeUnit.MILLISECONDS);
+  }
+
+  @Override
+  public void register(SpanContext spanContext) {
+    delegate.register(spanContext);
+    traceIds.put(spanContext.getTraceId(), new RegistrationContext(Instant.now(), spanContext));
+  }
+
+  @Override
+  public boolean isRegistered(SpanContext spanContext) {
+    return delegate.isRegistered(spanContext);
+  }
+
+  @Override
+  public void unregister(SpanContext spanContext) {
+    delegate.unregister(spanContext);
+  }
+
+  @Override
+  public void close() throws Exception {
+    scheduler.shutdown();
+  }
+
+  private Runnable removeStalledTraces(Duration stalledTimeLimit) {
+    return () ->
+        traceIds
+            .entrySet()
+            .iterator()
+            .forEachRemaining(
+                entry -> {
+                  Instant now = Instant.now();
+                  RegistrationContext context = entry.getValue();
+                  Duration duration = Duration.between(now, context.registrationTime);
+                  if (duration.compareTo(stalledTimeLimit) <= 0) {
+                    unregister(context.spanContext);
+                  }
+                });
+  }
+
+  private static class RegistrationContext {
+    private final Instant registrationTime;
+    private final SpanContext spanContext;
+
+    private RegistrationContext(Instant registrationTime, SpanContext spanContext) {
+      this.registrationTime = registrationTime;
+      this.spanContext = spanContext;
+    }
+  }
+}
---
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/StalledTraceDetectingTraceRegistryTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.splunk.opentelemetry.profiler.snapshot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import java.time.Duration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class StalledTraceDetectingTraceRegistryTest {
+  private final TraceRegistry delegate = new SimpleTraceRegistry();
+  private final StalledTraceDetectingTraceRegistry registry =
+      new StalledTraceDetectingTraceRegistry(delegate);
+
+  @AfterEach
+  void teardown() throws Exception {
+    registry.close();
+  }
+
+  @Test
+  void delegateTraceRegistration() {
+    var spanContext = Snapshotting.spanContext().build();
+    registry.register(spanContext);
+    assertThat(delegate.isRegistered(spanContext)).isTrue();
+  }
+
+  @Test
+  void delegateTraceDeregistration() {
+    var spanContext = Snapshotting.spanContext().build();
+
+    registry.register(spanContext);
+    registry.unregister(spanContext);
+
+    assertThat(delegate.isRegistered(spanContext)).isFalse();
+  }
+
+  @Test
+  void delegateTraceRegistrationLookup() {
+    var spanContext = Snapshotting.spanContext().build();
+    delegate.register(spanContext);
+    assertThat(registry.isRegistered(spanContext)).isTrue();
+  }
+
+  @Test
+  void removeRegisteredTracesAfterStalledTimeLimitPasses() throws Exception {
+    var spanContext = Snapshotting.spanContext().build();
+    var stalledTimeLimit = Duration.ofMillis(10);
+
+    try (var registry = new StalledTraceDetectingTraceRegistry(delegate, stalledTimeLimit)) {
+      registry.register(spanContext);
+      assertThat(registry.isRegistered(spanContext)).isTrue();
+
+      await().untilAsserted(() -> assertThat(delegate.isRegistered(spanContext)).isFalse());
+    }
+  }
+}

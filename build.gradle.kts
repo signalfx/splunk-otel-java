00b938ba3926980631164bf2403994b1b9b54b3d
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2025-06-06 13:50:33 -0700

    update to new sonatype central urls (#2331)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,10 @@ nexusPublishing {
   packageGroup.set("com.splunk")
 
   repositories {
+    // see https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#configuration
     sonatype {
+      nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+      snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
       username.set(System.getenv("SONATYPE_USER"))
       password.set(System.getenv("SONATYPE_KEY"))
     }

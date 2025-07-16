6c2898dfe93832d755079a58d56e60d461f60b47
thomasduncan <tduncan@splunk.com>
2025-07-16 09:21:29 -0700

    Simplify ContextStorageResettingSpanTrackingActivator by having reuse ContextStorage wrapping code in InterceptingContextStorageSpanTrackingActivator.

--- profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/ContextStorageResettingSpanTrackingActivator.java
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/ContextStorageResettingSpanTrackingActivator.java
@@ -21,14 +21,12 @@ import io.opentelemetry.sdk.testing.context.SettableContextStorageProvider;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-class ContextStorageResettingSpanTrackingActivator
-    implements SpanTrackingActivator, AfterEachCallback {
-  @Override
-  public void activate(TraceRegistry registry) {
-    ActiveSpanTracker spanTracker =
-        new ActiveSpanTracker(ContextStorage.defaultStorage(), registry);
-    SpanTracker.SUPPLIER.configure(spanTracker);
-    SettableContextStorageProvider.setContextStorage(spanTracker);
+class ContextStorageResettingSpanTrackingActivator extends InterceptingContextStorageSpanTrackingActivator implements AfterEachCallback {
+  ContextStorageResettingSpanTrackingActivator() {
+    super(operator -> {
+      var storage = operator.apply(ContextStorage.defaultStorage());
+      SettableContextStorageProvider.setContextStorage(storage);
+    });
   }
 
   @Override

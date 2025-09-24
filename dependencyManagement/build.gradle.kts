509b385ce4a5e346e8b6c4176e125a76710ea197
Lauri Tulmin <ltulmin@splunk.com>
2025-09-24 15:26:26 +0300

    use dependency management

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -56,5 +56,8 @@ dependencies {
     api("io.opentelemetry.contrib:opentelemetry-samplers:$otelContribAlphaVersion")
     api("io.opentelemetry.contrib:opentelemetry-resource-providers:$otelContribAlphaVersion")
     api("io.opentelemetry.proto:opentelemetry-proto:1.8.0-alpha")
+
+    api("org.apache.commons:commons-jexl3:3.5.0")
+    api("org.snakeyaml:snakeyaml-engine:2.10")
   }
 }

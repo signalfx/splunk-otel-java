656ea78a577d5b4b3f8605e732c9e8b94e4719ef
Robert Niedziela <175605712+robsunday@users.noreply.github.com>
2025-10-16 16:31:38 +0200

    [File based config] Port of SplunkConfiguration class (#2479)
    * Port of SplunkConfiguration class
    
    * Small code and docs updates
    
    * Code review followup
    
    * Code review followup

--- testing/common/build.gradle.kts
+++ testing/common/build.gradle.kts
@@ -8,4 +8,18 @@ dependencies {
   api("io.opentelemetry.javaagent:opentelemetry-testing-common")
   api("com.squareup.okhttp3:okhttp")
   api("org.mockito:mockito-core")
+  api("io.opentelemetry:opentelemetry-sdk-extension-incubator")
+  api("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
+  api("io.opentelemetry.instrumentation:opentelemetry-declarative-config-bridge")
+}
+
+tasks {
+  java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  compileTestJava {
+    sourceCompatibility = JavaVersion.VERSION_17.toString()
+    targetCompatibility = JavaVersion.VERSION_17.toString()
+  }
 }

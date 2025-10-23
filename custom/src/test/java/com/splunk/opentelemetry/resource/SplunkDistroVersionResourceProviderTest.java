1818fcfbcb1d72424aa786740eb96b1d5d93969c
robsunday <rniedziela@splunk.com>
2025-10-23 10:09:41 +0200

    SplunkDistroVersionResourceDetector

--- custom/src/test/java/com/splunk/opentelemetry/resource/SplunkDistroVersionResourceProviderTest.java
+++ custom/src/test/java/com/splunk/opentelemetry/resource/SplunkDistroVersionResourceProviderTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.splunk.opentelemetry;
+package com.splunk.opentelemetry.resource;
 
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
 import static io.opentelemetry.semconv.incubating.TelemetryIncubatingAttributes.TELEMETRY_DISTRO_VERSION;

ca2267e3ee970b9f6daac1d4fe27b1889ecc9a80
robsunday <rniedziela@splunk.com>
2026-01-19 18:54:57 +0100

    Tests added

--- profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/StackTraceExporterActivatorTest.java
+++ profiler/src/test/java/com/splunk/opentelemetry/profiler/snapshot/StackTraceExporterActivatorTest.java
@@ -16,19 +16,28 @@
 
 package com.splunk.opentelemetry.profiler.snapshot;
 
+import static com.splunk.opentelemetry.testing.declarativeconfig.DeclarativeConfigTestUtil.createAutoConfiguredSdk;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import io.opentelemetry.instrumentation.testing.internal.AutoCleanupExtension;
 import io.opentelemetry.sdk.autoconfigure.OpenTelemetrySdkExtension;
+import java.io.IOException;
+import java.nio.file.Path;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.io.TempDir;
 
 class StackTraceExporterActivatorTest {
   @AfterEach
   void tearDown() {
+    SpanTracker.SUPPLIER.reset();
+    StackTraceSampler.SUPPLIER.reset();
+    StagingArea.SUPPLIER.reset();
+    SnapshotProfilingDeclarativeConfiguration.SUPPLIER.reset();
     StackTraceExporter.SUPPLIER.reset();
   }
 
@@ -60,6 +69,50 @@ class StackTraceExporterActivatorTest {
 
     @Test
     void doNotConfigureStackTraceExporterProvider() {
+      var exporter = StackTraceExporter.SUPPLIER.get();
+      assertSame(StackTraceExporter.NOOP, exporter);
+    }
+  }
+
+  @Nested
+  class DeclarativeConfig {
+    @RegisterExtension final AutoCleanupExtension autoCleanup = AutoCleanupExtension.create();
+
+    @Test
+    void configureStackTraceExporterProvider(@TempDir Path tempDir) throws IOException {
+      String yaml =
+          """
+            file_format: "1.0-rc.3"
+            distribution:
+              splunk:
+                profiling:
+                  exporter:
+                    otlp_log_http:
+                  callgraphs:
+            """;
+      var sdk = createAutoConfiguredSdk(yaml, tempDir, autoCleanup);
+
+      new StackTraceExporterActivator().afterAgent(sdk);
+
+      var exporter = StackTraceExporter.SUPPLIER.get();
+      assertNotSame(StackTraceExporter.NOOP, exporter);
+      assertInstanceOf(AsyncStackTraceExporter.class, exporter);
+    }
+
+    @Test
+    void doNotConfigureStackTraceExporterProviderWhenNoCallgraphs(@TempDir Path tempDir)
+        throws IOException {
+      String yaml =
+          """
+            file_format: "1.0-rc.3"
+            distribution:
+              splunk:
+                profiling:
+            """;
+      var sdk = createAutoConfiguredSdk(yaml, tempDir, autoCleanup);
+
+      new StackTraceExporterActivator().afterAgent(sdk);
+
       var exporter = StackTraceExporter.SUPPLIER.get();
       assertSame(StackTraceExporter.NOOP, exporter);
     }

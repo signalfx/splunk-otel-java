cd129e846a2f176df60f1102fd9c5605acc65d80
Lauri Tulmin <ltulmin@splunk.com>
2026-03-03 20:25:18 +0200

    Update smoke test image version (#2679)
    * Update smoke test image version
    
    * fix jetty and tomcat versions
    
    * fix version
    
    * fix tomcat version

--- smoke-tests/src/test/java/com/splunk/opentelemetry/TomcatSmokeTest.java
+++ smoke-tests/src/test/java/com/splunk/opentelemetry/TomcatSmokeTest.java
@@ -30,21 +30,21 @@ public class TomcatSmokeTest extends AppServerTest {
   public static final ExpectedServerAttributes TOMCAT8_SERVER_ATTRIBUTES =
       new TomcatAttributes("8.5.98.0");
   public static final ExpectedServerAttributes TOMCAT9_SERVER_ATTRIBUTES =
-      new TomcatAttributes("9.0.85.0");
+      new TomcatAttributes("9.0.111.0");
   public static final ExpectedServerAttributes TOMCAT10_SERVER_ATTRIBUTES =
-      new TomcatAttributes("10.1.18.0");
+      new TomcatAttributes("10.1.48.0");
 
   private static Stream<Arguments> supportedConfigurations() {
     return configurations("tomcat")
         .otelLinux("7.0.109", TOMCAT7_SERVER_ATTRIBUTES, VMS_HOTSPOT, "8")
         .otelLinux("8.5.98", TOMCAT8_SERVER_ATTRIBUTES, VMS_ALL, "8", "11")
-        .otelLinux("9.0.85", TOMCAT9_SERVER_ATTRIBUTES, VMS_HOTSPOT, "8", "11")
-        .otelLinux("10.1.18", TOMCAT10_SERVER_ATTRIBUTES, VMS_HOTSPOT, "11", "17", "21")
-        .otelLinux("10.1.18", TOMCAT10_SERVER_ATTRIBUTES, VMS_OPENJ9, "11")
+        .otelLinux("9.0.111", TOMCAT9_SERVER_ATTRIBUTES, VMS_HOTSPOT, "8", "11")
+        .otelLinux("10.1.48", TOMCAT10_SERVER_ATTRIBUTES, VMS_HOTSPOT, "11", "17", "21")
+        .otelLinux("10.1.48", TOMCAT10_SERVER_ATTRIBUTES, VMS_OPENJ9, "11")
         .otelWindows("7.0.109", TOMCAT7_SERVER_ATTRIBUTES, VMS_HOTSPOT, "8")
         .otelWindows("8.5.98", TOMCAT8_SERVER_ATTRIBUTES, VMS_HOTSPOT, "8", "11")
-        .otelWindows("9.0.85", TOMCAT9_SERVER_ATTRIBUTES, VMS_HOTSPOT, "8", "11")
-        .otelWindows("10.1.18", TOMCAT10_SERVER_ATTRIBUTES, VMS_HOTSPOT, "11", "17", "21")
+        .otelWindows("9.0.111", TOMCAT9_SERVER_ATTRIBUTES, VMS_HOTSPOT, "8", "11")
+        .otelWindows("10.1.48", TOMCAT10_SERVER_ATTRIBUTES, VMS_HOTSPOT, "11", "17", "21")
         .stream();
   }
 

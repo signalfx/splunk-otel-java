8fdd1d574075d0deaeb4391266068144da290b0b
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2026-03-14 05:11:09 -0700

    Fix khttp client instrumentation (#2705)
    * port is required now, so default back to scheme if needed.
    
    * simplify
    
    ---------
    
    Co-authored-by: Lauri Tulmin <ltulmin@splunk.com>

--- instrumentation/khttp/src/main/java/com/splunk/opentelemetry/instrumentation/khttp/KHttpHttpClientHttpAttributesGetter.java
+++ instrumentation/khttp/src/main/java/com/splunk/opentelemetry/instrumentation/khttp/KHttpHttpClientHttpAttributesGetter.java
@@ -19,7 +19,9 @@ package com.splunk.opentelemetry.instrumentation.khttp;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 
+import io.opentelemetry.instrumentation.api.internal.HttpConstants;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpClientAttributesGetter;
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -74,10 +76,10 @@ final class KHttpHttpClientHttpAttributesGetter
   @Nullable
   @Override
   public Integer getServerPort(RequestWrapper requestWrapper) {
-    if (requestWrapper.parsedUri != null && requestWrapper.parsedUri.getPort() > 0) {
-      return requestWrapper.parsedUri.getPort();
-    }
-    return null;
+    URI uri = requestWrapper.parsedUri;
+    return uri != null
+        ? HttpConstants.portOrDefaultFromScheme(uri.getPort(), uri.getScheme())
+        : null;
   }
 
   @Nullable

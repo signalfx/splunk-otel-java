7e8340723d5a85c3069f0788867eab11885e091e
Jason Plumb <jplumb@splunk.com>
2026-08-11 16:02:36 -0700

    help renovate find csa

--- agent-csa-bundle/build.gradle.kts
+++ agent-csa-bundle/build.gradle.kts
@@ -6,8 +6,7 @@ plugins {
   id("com.gradleup.shadow")
 }
 
-// This should be updated for every CSA release, eventually in dependencyManagement?
-
+// Updated from the latest signalfx/csa-releases GitHub release by Renovate.
 val csaVersion = "26.8.0-1482"
 val otelInstrumentationVersion: String = rootProject.extra["otelInstrumentationVersion"] as String
 
